5b68063cb9c2490d022bbccef124d8bf581d3cd8
jskott7 <jskott@syr.edu>
2025-05-16 00:11:20 -0400

    Update final_project.R
    I added the poverty table, messed around with it to be in year/state/poverty rate format and then I merged the three datasets together.

--- final_project.R
+++ final_project.R
@@ -1,20 +1,54 @@
+library(lubridate)
+library(janitor) 
 library(tidyverse)
 library(readxl)
-
+library(dplyr)
+library(stringr)
 # Import datasets to variable names
 
-crimeData <- read.csv("C:/Users/aaron/Documents/Syracuse/IST 687 Intro to Data Science/Datasets/US_Crime_DataSet.csv")
-minWage <- read.csv("C:/Users/aaron/Documents/Syracuse/IST 687 Intro to Data Science/Datasets/Minimum Wage Data.csv")
+crimeData <- read.csv("US_Crime_DataSet.csv")
+minWage <- read.csv("Minimum Wage Data.csv")
+povertyRate <- read.csv("Poverty_rate_CPS.csv")
 
 # Evaluate the structure of the datasets
 
 str(crimeData)
 str(minWage)
+str(povertyRate)
 
 # Clean attribute names
 
 crimeData <- crimeData %>% janitor::clean_names()
 minWage <- minWage %>% janitor::clean_names()
+povertyRate <- povertyRate %>% janitor::clean_names()
+
+# Add a year attribute to povertyRate
+povertyRate <- povertyRate %>%
+  rename(temp_date = date)
+povertyRate$year <- as.numeric(format(as.Date(povertyRate$temp_date), "%Y"))
+povertyRate$temp_date <- NULL
+
+# Pivot povertyRate to match state attributes 
+povertyRate_long <- povertyRate %>%
+  pivot_longer(
+    cols = -year,
+    names_to = "state",
+    values_to = "poverty rate"
+  )
+
+
+# Group by year and state, then filter timeframe
+povertyRate <- povertyRate_long %>%
+  filter(
+    year %in% c(1980:2015),
+    !is.na(state),
+    !is.na(year),
+    !state %in% c("district_of_columbia", "united_states")
+  ) %>%
+  select(year, state, `poverty rate`) %>%
+  group_by(year, state)
+
+povertyRate$state <- str_to_title(povertyRate$state)
 
 # Group homicide by state and then by year for 20 years
 crime_by_year <- crimeData %>%
@@ -33,7 +67,7 @@ min_wage_by_year <- minWage %>%
 new_york_crime <- crime_by_year %>% filter(state == "New York")
 texas_crime <- crime_by_year %>% filter(state == "Texas")
 california_crime <- crime_by_year %>% filter(state == "California")
-florida_crime <- crime_by_year %>% filter(state = "Florida")
+florida_crime <- crime_by_year %>% filter(state == "Florida")
 pennsylvania_crime <- crime_by_year %>% filter(state == "Pennsylvania")
 
 # Create minimum wage dataframes for the five most populous states
@@ -44,5 +78,10 @@ florida_min_wage <- min_wage_by_year %>% filter(state == "Florida")
 pennsylvania_min_wage <- min_wage_by_year %>% filter(state == "Pennsylvania")
 
 
+
 # Join dataframes 
-# look into poverty rate
+merged_df <- povertyRate %>%
+  left_join(crimeData, by = c("state", "year")) %>%
+  left_join(minWage, by = c("state", "year"))
+
+head(merged_df)
